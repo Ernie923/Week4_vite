961f8638b6572383009f9b48786f9122bf0f0059
Ernie923 <hanchen923@hotmail.com>
2024-08-08 16:06:18 +0800

    first commit

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法：
   // base: '/Repository 的名稱/'
-  base: '/Week4_vite/',
+  base: '/week4_vite/',
   plugins: [
     liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
     ViteEjsPlugin(),
